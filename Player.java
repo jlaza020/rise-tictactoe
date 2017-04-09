a52893a3b88a3ba4e1fe8914d42fd35b19a25d9f
Jahkell Lazarre <jlaza020@fiu.edu>
2017-04-09 17:04:30 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,58 @@
+package TicTacToe;
+
+/**
+ * A player in a game of TicTacToe.
+ */
+public class Player {
+    
+    private String name;
+    private char symbol;
+    
+    /**
+     * Constructs a player object.
+     * 
+     * @param name Name of the player.
+     */
+    public Player (String name){
+    
+        // TODO
+    
+    }
+    
+    /**
+     * Gets name of player.
+     * 
+     * @return Name of player.
+     */
+    public String getName(){
+    
+        // TODO
+        
+        return null;
+    }
+    
+    /**
+     * Gets player's TicTacToe symbol.
+     * 
+     * @return This player's symbol.
+     */
+    public char getSymbol(){
+    
+        // TODO
+        
+        return 'a';
+    
+    }
+    
+    /**
+     * Sets player's TicTacToe symbol.
+     * 
+     * @param symbol Symbol to set for player.
+     */
+    public void setSymbol(char symbol){
+    
+        // TODO
+    
+    }
+    
+}
